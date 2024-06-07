697e3a6871fa125dd99133e3dd48f0777404dc10
Zhangjin Xiao <zhangjin.xiao94@gmail.com>
2024-06-07 08:51:06 +0800

    edit settings

--- costcalc/settings.py
+++ costcalc/settings.py
@@ -13,26 +13,27 @@ else:
 class BaseConfig:
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev key')
 
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', prefix + os.path.join(basedir, 'data.db'))
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_RECORD_QUERIES = True
 
 
 class DevelopmentConfig(BaseConfig):
-    pass
-
-
-class ProductionConfig(BaseConfig):
-    pass
+    SQLALCHEMY_DATABASE_URI = prefix + os.path.join(basedir, 'data-dev.db')
 
 
 class TestingConfig(BaseConfig):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///'
     WTF_CSRF_ENABLED = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+
+class ProductionConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = prefix + os.path.join(basedir, os.getenv('DATABASE_FILE', 'data-prod.db'))
 
 
 config = {
     'development': DevelopmentConfig,
-    'production': ProductionConfig,
-    'testing': TestingConfig
+    'testing': TestingConfig,
+    'production': ProductionConfig
 }
